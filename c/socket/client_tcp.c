a7fb9c1415d969af35d97bd6bd39ba31bca135b4
ilcors-dev <corsetti.luca28@gmail.com>
2022-07-10 14:07:08 +0200

    added: client_tcp & client_udp template files

---
+++ c/socket/client_tcp.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include "../multilib.h"
+
+int main(int argc, char *argv[])
+{
+	// socket handling variables
+	int port, nread, socket_d, nwrite;
+	char userInput[STR_LEN];
+	struct hostent *host;
+	struct sockaddr_in servaddr;
+
+	// your data here
+
+	// ARGS CHECK -------------------------------------------------------------------
+	if (argc != 3)
+	{
+		printf("Error:%s serverAddress serverPort\n", argv[0]);
+		exit(1);
+	}
+
+	if (is_numeric_string(argv[2]) == 0)
+	{
+		printf("Error:%s serverPort must be a number\n", argv[2]);
+		exit(1);
+	}
+
+	port = atoi(argv[2]);
+	if (is_port_valid(port) == 0)
+	{
+		printf("Error:%s serverPort must be in the range 1024-65535\n", argv[2]);
+		exit(2);
+	}
+
+	// server tcp address initialization
+	memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
+	servaddr.sin_family = AF_INET;
+	host = gethostbyname(argv[1]);
+	if (host == NULL)
+	{
+		printf("%s not found in /etc/hosts\n", argv[1]);
+		exit(1);
+	}
+
+	servaddr.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
+	servaddr.sin_port = htons(port);
+
+	// !NOTE THE CONNECTION TO THE SERVER IS ESTABLISHED OUTSIDE OF THE LOOP, THIS MAY VARY DEPENDING ON THE
+	// !SERVER'S REQUIREMENTS IMPLEMENTATION
+
+	// creating socket
+	socket_d = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_d < 0)
+	{
+		perror("Could not create socket");
+		exit(3);
+	}
+	printf("Successfully created socket, socket_d=%d\n", socket_d);
+
+	// bind connect
+	if (connect(socket_d, (struct sockaddr *)&servaddr, sizeof(struct sockaddr)) < 0)
+	{
+		perror("Could not connect to server");
+		exit(3);
+	}
+	printf("Connection established successfully\n");
+
+	// !NOTE END
+
+	// user interaction
+
+	printf("Waiting user input...\n");
+	while (gets(userInput))
+	{
+		// handle server interaction
+		int whatever;
+		printf("Sending to server: %s\n", userInput);
+
+		nwrite = write(socket_d, userInput, strlen(userInput));
+
+		while ((nread = read(socket_d, &whatever, sizeof(int))) > 0)
+		{
+			printf("%d\n", whatever);
+		}
+
+		printf("Waiting user input...\n");
+		// -------------------------
+	}
+	close(socket_d);
+	printf("Client, shutting down...");
+	exit(0);
+}
---
+++ c/socket/client_udp.c
@@ -0,0 +1,113 @@
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "../multilib.h"
+
+typedef struct
+{
+	// ...
+} ReqUDP;
+
+int main(int argc, char **argv)
+{
+	struct hostent *host;
+	struct sockaddr_in clientaddr, servaddr;
+	int port, socked_d, len = 0;
+	char userInput[STR_LEN];
+	// ReqUDP req;
+
+	// ARGS CHECK -------------------------------------------------------------------
+	if (argc != 3)
+	{
+		printf("Error:%s serverAddress serverPort\n", argv[0]);
+		exit(1);
+	}
+
+	if (is_numeric_string(argv[2]) == 0)
+	{
+		printf("Error:%s serverPort must be a number\n", argv[2]);
+		exit(1);
+	}
+
+	port = atoi(argv[2]);
+	if (is_port_valid(port) == 0)
+	{
+		printf("Error:%s serverPort must be in the range 1024-65535\n", argv[2]);
+		exit(2);
+	}
+
+	// client address initialization
+	memset((char *)&clientaddr, 0, sizeof(struct sockaddr_in));
+	clientaddr.sin_family = AF_INET;
+	clientaddr.sin_addr.s_addr == INADDR_ANY;
+	clientaddr.sin_port = 0;
+
+	// server address initialization
+	memset((char *)&servaddr, 0, sizeof(struct sockaddr_in));
+	servaddr.sin_family = AF_INET;
+	host = gethostbyname(argv[1]);
+
+	// host check
+	if (host == NULL)
+	{
+		printf("%s not found in /etc/hosts\n", argv[1]);
+		exit(2);
+	}
+	else
+	{
+		servaddr.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
+		servaddr.sin_port = htons(port);
+	}
+
+	// socket creation
+	socked_d = socket(AF_INET, SOCK_DGRAM, 0);
+	if (socked_d < 0)
+	{
+		perror("Cloud not open udp socket");
+		exit(1);
+	}
+	printf("Successfully created udp socket, socked_d=%d\n", socked_d);
+
+	// socket binding
+	if (bind(socked_d, (struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0)
+	{
+		perror("bind socket ");
+		exit(1);
+	}
+	printf("Successfully bind socket at port %i\n", clientaddr.sin_port);
+
+	// user interaction
+
+	printf("Waiting user input...\n");
+	while (gets(userInput))
+	{
+		int whatever;
+		len = sizeof(servaddr);
+		if (sendto(socked_d, userInput, sizeof(userInput), 0, (struct sockaddr *)&servaddr, len) < 0)
+		{
+			perror("Could not send message with sendto");
+			// if error happens, continue cycle anyway
+			printf("Waiting user input...\n");
+			continue;
+		}
+
+		if (recvfrom(socked_d, &whatever, sizeof(whatever), 0, (struct sockaddr *)&servaddr, &len) < 0)
+		{
+			perror("Could not receive message with recvfrom");
+			// if error happens, continue cycle anyway
+			printf("Waiting user input...\n");
+			continue;
+		}
+
+		printf("Result: %i\n", whatever);
+		printf("Waiting user input...\n");
+	}
+
+	close(socked_d);
+}
